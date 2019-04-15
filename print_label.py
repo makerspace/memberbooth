0056371ca2b3cd3a929f31cf2d37cb90d97795d6
Andreas Lundquist <lundquist.andreas@gmail.com>
2019-04-15 20:59:58 +0200

    Quick hack to make print_label.py

--- print_label.py
+++ print_label.py
@@ -4,25 +4,44 @@ import argparse
 
 from src import label_creator
 from src import label_printer
-
-NAME_KEY = 'name'
-MEMBER_NUMBER_KEY = 'member_number'
+from src import maker_admin
+from src.member import Member
+from time import time
+import config 
 
 def main():
 
     parser = argparse.ArgumentParser(description='Print label for project box')
-
-    parser.add_argument(MEMBER_NUMBER_KEY,
-                        type=int,
-                        help='The member number of the owner of the project box')
-
-    parser.add_argument(f'--{NAME_KEY}',
+    parser.add_argument("token", help="Makeradmin token")
+    parser.add_argument("-u", "--maker-admin-base-url",
+                        default='https://api.makeradmin.se',
+                        help="Base url of maker admin (for login and fetching of member info).)")
+    
+    parser.add_argument("--debug", action="store_true", help="Do not send requests to the backend")
+    parser.add_argument("--no_printing", action="store_true", help="Image is sent to label printer, instead the image is saved to working directory")
+    
+    parser.add_argument('member_number',
                         type=str,
-                        help='The name of the owner of the project box')
+                        help='The member number of the member you want to print a label for')
+    config.ns = parser.parse_args()
+    
+    makeradmin_client = maker_admin.MakerAdminClient(base_url=config.ns.maker_admin_base_url, token=config.ns.token)
 
-    args = vars(parser.parse_args())
+    logged_in = makeradmin_client.is_logged_in()
+    print("Logged in: ", logged_in)
+    if not logged_in:
+        return
 
-    label = label_creator.create_box_label(args[MEMBER_NUMBER_KEY], args[NAME_KEY])
+    member = Member.from_member_number(makeradmin_client, config.ns.member_number)
+
+    label = label_creator.create_box_label(member.member_number, member.get_name())
+    
+    if config.ns.no_printing:
+        file_name = f'{member.member_number}_{str(int(time()))}.png'
+        print(f'Program run with --no_printing, storing image to {file_name} instead of printing it.')
+        label.save(file_name)
+        return
+    
     label_printer.print_label(label)
 
 if __name__== "__main__":
--- src/member.py
+++ src/member.py
@@ -10,6 +10,9 @@ class Member(object):
     def get_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def __str__(self):
+        return f'{self.member_number}, {self.get_name()}, {self.lab_end_date}'
+
     @classmethod
     def from_tagid(cls, client, tagid):
         data = client.get_tag_info(tagid)
@@ -22,4 +25,15 @@ class Member(object):
             lab_end_date = dateutil.parser.parse(lab_end_date)
         return cls(member_data["firstname"], member_data["lastname"], member_data["member_number"], lab_end_date)
 
+    @classmethod
+    def from_member_number(cls, client, member_number):
+        data = client.get_member_number_info(member_number)
+        if data["data"] is None:
+            raise Exception(f"No key/member associated with tagid {tagid}")
+        
+        member_data = data["data"]
+        lab_end_date = member_data["expire_date"]
+        if lab_end_date is not None:
+            lab_end_date = dateutil.parser.parse(lab_end_date)
+        return cls(member_data["name"], '',  member_number, lab_end_date)
 
