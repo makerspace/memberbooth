ce431847899e0c74d3f3c51e332cbee5cdf0633e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-04-20 11:39:24 +0200

    Mocking get_member_number_info as well

--- src/test/maker_admin_mock.py
+++ src/test/maker_admin_mock.py
@@ -1,5 +1,17 @@
 import datetime
 
+response = {
+    "data": {
+        "member": {
+            "end_date": "2019-04-19",
+            "member_number": 9999,
+            "firstname": "Firstname",
+            "lastname": "Lastname"
+        },
+    },
+    "status": "success"
+}
+
 class MakerAdminClient(object):
     def __init__(self, *args, **kwargs):
         pass
@@ -8,14 +20,7 @@ class MakerAdminClient(object):
         return True
 
     def get_tag_info(self, tagid):
-        return {
-            "data": {
-                "member": {
-                    "end_date": "2019-04-19",
-                    "member_number": 9999,
-                    "firstname": "Firstname",
-                    "lastname": "Lastname"
-                },
-            },
-            "status": "success"
-        }
+        return response
+
+    def get_member_number_info(self, member_number):
+        return response
