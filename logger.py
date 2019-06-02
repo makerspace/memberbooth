8d6fe390f9e59201c7fc0ffb127ac2e557914185
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-06-02 18:59:47 +0200

    Splitting logging to file and stderr
    Closes #24

--- logger.py
+++ logger.py
@@ -1,11 +1,25 @@
-from logging import getLogger, INFO, basicConfig
+import logging, logging.handlers
 import sys
 
 LOGGER_NAME = 'memberbooth'
 
 def init_logger(logger_name=LOGGER_NAME):
-    logger = getLogger(logger_name)
-    basicConfig(format='%(asctime)s %(levelname)s [%(process)d/%(threadName)s %(pathname)s:%(lineno)d]: %(message)s', stream=sys.stderr, level=INFO)
+    logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s [%(process)d/%(threadName)s %(pathname)s:%(lineno)d]: %(message)s')
+
+    # Logger to file with rotating backups and high verbosity
+    fh = logging.handlers.RotatingFileHandler(logger_name + ".log", maxBytes=1e6, backupCount=5)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # Logger to stderr
+    sh = logging.StreamHandler(sys.stderr)
+    sh.setLevel(logging.WARNING)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
     
 def get_logger(logger_name=LOGGER_NAME):
-    return getLogger(logger_name)
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    return logger
