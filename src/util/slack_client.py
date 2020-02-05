913dca773e3be1466e393c871f11342706a8e7c5
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-02-05 23:59:31 +0100

    Class for configuring clients with tokens from files

--- src/util/slack_client.py
+++ src/util/slack_client.py
@@ -3,62 +3,43 @@ import os
 import slack
 import config
 from pathlib import Path
+from src.util.token_config import TokenConfiguredClient, TokenExpiredError
 
 logger = get_logger()
 
-class SlackTokenExpiredError(ValueError):
+class SlackTokenExpiredError(TokenExpiredError):
     pass
 
-class SlackClient():
+class SlackClient(TokenConfiguredClient):
     def __init__(self, token_path, channel_id, token=None):
-        self.configured = False
         self.client = slack.WebClient(token)
         self.token_path = token_path
         self.channel_id = channel_id
         if token:
-            self.configure(token)
+            self.configure_client(token)
 
-    def configure(self, token):
+    def configure_client(self, token):
         self.client.token = token
-        self.configured = True
 
-    def check_configured(self):
-        if not self.configured and Path(self.token_path).is_file():
-            with open(self.token_path) as f:
-                token = f.read()
-            self.configure(token)
-            try:
-                self._post_message("Slack client reconnected")
-                logger.info("Slack client reconnected")
-            except SlackTokenExpiredError:
-                logger.error("Slack token file expired. Removing it.")
-                try:
-                    os.remove(self.token_path)
-                except FileNotFoundError:
-                    pass
+    def try_log_in(self):
+        self._post_message("Slack client reconnected")
 
     def _post_message(self, msg):
-        if not self.configured:
-            return
-
         try:
             response = self.client.chat_postMessage(
                 channel=self.channel_id,
                 text=msg,
                 link_names=True)
         except slack.errors.SlackApiError as e:
-            self.configured = False
             logger.error(f"SlackClient failed with error: {e}")
             raise SlackTokenExpiredError(str(e))
 
+    @TokenConfiguredClient.require_configured
     def post_message(self, msg):
-        self.check_configured()
-
         try:
             self._post_message(msg)
         except SlackTokenExpiredError:
             logger.error("Slack token is not valid anymore")
-
 
     def post_message_info(self, msg):
         self.post_message(msg)
---
+++ src/util/token_config.py
@@ -0,0 +1,52 @@
+from src.util.logger import get_logger
+import os
+from pathlib import Path
+
+logger = get_logger()
+
+class TokenExpiredError(ValueError):
+    pass
+
+class TokenConfiguredClient(object):
+    _configured = False
+    token_path = None
+
+    def configure_client(self, token):
+        '''
+        Gets called with the token when it becomes available
+        '''
+        raise NotImplemented()
+
+    def try_log_in(self):
+        '''
+        Check if logged in. Raise a TokenExpiredError if not logged in
+        '''
+        raise NotImplemented()
+
+    @property
+    def configured(self):
+        if not self._configured and Path(self.token_path).is_file():
+            with open(self.token_path) as f:
+                token = f.read()
+            self.configure_client(token)
+            try:
+                self.try_log_in()
+                self._configured = True
+                logger.info("Client logged in")
+            except TokenExpiredError:
+                logger.error("Token file expired. Removing it.")
+                try:
+                    os.remove(self.token_path)
+                except FileNotFoundError:
+                    pass
+                self._configured = False
+
+        return self._configured
+
+    def require_configured(f, default_retval=None):
+        def require_configured_wrapper(self, *args, **kwargs):
+            if not self.configured:
+                return default_retval
+            return f(*args, **kwargs)
+        return require_configured_wrapper
+
