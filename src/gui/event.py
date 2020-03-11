d6ac33569bc93c57d75227e9ab62c04be0708a0e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-03-11 13:45:44 +0100

    Automatic naming of events

--- src/gui/event.py
+++ src/gui/event.py
@@ -1,10 +1,16 @@
-from enum import Enum, unique
+from enum import Enum, unique, auto
 
 class WrongEventData(TypeError):
     pass
 
 @unique
 class BaseEvent(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        '''
+        Overrides the value returned by the auto() function
+        '''
+        return name
+
     def __init__(self, event, datatype=None):
         self.event = event
         self.datatype = datatype
@@ -33,26 +39,26 @@ class BaseEvent(Enum):
 
 
 class Event(BaseEvent):
-    MAKERADMIN_CLIENT_CONFIGURED = ('makeradmin_configured', None)
-    TAG_READ = ('tag_read', str)
-    MEMBER_INFORMATION_RECEIVED = ('member_information_received', None)
-    LOG_OUT = ('log_out', None)
-    PRINT_STORAGE_BOX_LABEL = ('print_storage_box_label', None)
-    PRINT_TEMPORARY_STORAGE_LABEL = ('print_temporary_storage_label', None)
-    LABEL_PRINTED = ('label_printed', None)
-    CANCEL = ('cancel', None)
-    PRINTING_FAILED = ('printing_failed', None)
-    PRINTING_SUCCEEDED = ('printing_succeeded', None)
-    SERIAL_PORT_DISCONNECTED = ('serial_port_disconnected', None)
-    KEY_READER_CONNECTED = ('key_reader_connected', None)
+    MAKERADMIN_CLIENT_CONFIGURED = (auto(), None)
+    TAG_READ = (auto(), str)
+    MEMBER_INFORMATION_RECEIVED = (auto(), None)
+    LOG_OUT = (auto(), None)
+    PRINT_STORAGE_BOX_LABEL = (auto(), None)
+    PRINT_TEMPORARY_STORAGE_LABEL = (auto(), None)
+    LABEL_PRINTED = (auto(), None)
+    CANCEL = (auto(), None)
+    PRINTING_FAILED = (auto(), None)
+    PRINTING_SUCCEEDED = (auto(), None)
+    SERIAL_PORT_DISCONNECTED = (auto(), None)
+    KEY_READER_CONNECTED = (auto(), None)
 
 class GuiEvent(BaseEvent):
-    PRINT_TEMPORARY_STORAGE_LABEL = ('print_storage_label', str)
-    PRINT_BOX_LABEL = ('print_box_label', None)
-    PRINT_CHEMICAL_LABEL = ('print_chemical_label', None)
-    LOG_OUT = ('log_out', None)
-    TAG_READ = ('tag_read', str)
-    DRAW_STORAGE_LABEL_GUI = ('draw_storage_label', None)
-    CANCEL = ('cancel', None)
-    TIMEOUT_TIMER_EXPIRED = ('timeout_timer_expired', None)
+    PRINT_TEMPORARY_STORAGE_LABEL = (auto(), str)
+    PRINT_BOX_LABEL = (auto(), None)
+    PRINT_CHEMICAL_LABEL = (auto(), None)
+    LOG_OUT = (auto(), None)
+    TAG_READ = (auto(), str)
+    DRAW_STORAGE_LABEL_GUI = (auto(), None)
+    CANCEL = (auto(), None)
+    TIMEOUT_TIMER_EXPIRED = (auto(), None)
 
