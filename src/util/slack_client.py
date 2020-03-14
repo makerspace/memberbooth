33002eb11740d8a5c31a5b55e06eb78368eed17a
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-03-14 23:49:43 +0100

    Forgot to save token member

--- src/util/slack_client.py
+++ src/util/slack_client.py
@@ -19,7 +19,7 @@ class SlackClient(TokenConfiguredClient):
             self.configure_client(token)
 
     def configure_client(self, token):
-        self.client.token = token
+        self.token = self.client.token = token
 
     def try_log_in(self):
         self._post_message("Slack client reconnected")
