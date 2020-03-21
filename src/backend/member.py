6310e882c45e873a2d5211c61d8a9659487d90ed
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-03-21 17:33:20 +0100

    Checking for response parse errors

--- src/backend/member.py
+++ src/backend/member.py
@@ -13,6 +13,9 @@ class NoMatchingMemberNumber(KeyError):
     def __init__(self, member_number):
         super().__init__(f"No member associated with member number: {member_number}")
 
+class BackendParseError(KeyError):
+    pass
+
 @dataclass(frozen=True)
 class EndDate:
     is_active: bool
@@ -35,11 +38,11 @@ class Member(object):
         return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
-        return f'{self.member_number}, {self.get_name()}, {self.lab_end_date}'
+        return f'#{self.member_number}, "{self.get_name()}", {self.membership},{self.effective_labaccess}({self.labaccess}{self.special_labaccess})'
 
     @classmethod
     def from_response(cls, response_data):
-        if (response_data or response_data["data"]) is None:
+        if (response_data and response_data["data"]) is None:
             return None
 
         def datify(makeradmin_date):
@@ -47,17 +50,22 @@ class Member(object):
                 return None
             return datetime.datetime.combine(dateutil.parser.parse(makeradmin_date).date(), datetime.time(23, 59, 59))
 
-        data = response_data["data"]
-        membership_data = data["membership_data"]
+        try:
+            data = response_data["data"]
+            membership_data = data["membership_data"]
 
-        return cls(
-            data["firstname"], data["lastname"],
-            data["member_number"],
-            membership = EndDate(membership_data["membership_active"], datify(membership_data["membership_end"])),
-            labaccess = EndDate(membership_data["labaccess_active"], datify(membership_data["labaccess_end"])),
-            special_labaccess = EndDate(membership_data["special_labaccess_active"], datify(membership_data["special_labaccess_end"])),
-            effective_labaccess = EndDate(membership_data["effective_labaccess_active"], datify(membership_data["effective_labaccess_end"])),
-        )
+            member =  cls(
+                data["firstname"], data["lastname"],
+                data["member_number"],
+                membership = EndDate(membership_data["membership_active"], datify(membership_data["membership_end"])),
+                labaccess = EndDate(membership_data["labaccess_active"], datify(membership_data["labaccess_end"])),
+                special_labaccess = EndDate(membership_data["special_labaccess_active"], datify(membership_data["special_labaccess_end"])),
+                effective_labaccess = EndDate(membership_data["effective_labaccess_active"], datify(membership_data["effective_labaccess_end"])),
+            )
+        except Exception as e:
+            raise BackendParseError(str(e))
+
+        return member
 
     @classmethod
     def from_tagid(cls, client, tagid):
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,6 +1,12 @@
 import unittest
 from src.backend import member, makeradmin
 from src.test import makeradmin_mock
+from unittest.mock import patch
+import copy
+
+# Make a deep copy of an object and then restore it afterwards
+def patch_deep_copy(target, template):
+    return patch(target, new_callable=copy.deepcopy, x=template)
 
 class TestMock(unittest.TestCase):
     def setUp(self):
@@ -8,3 +14,22 @@ class TestMock(unittest.TestCase):
 
     def test_parse_response(self):
         m = member.Member.from_tagid(self.client, 0)
+
+    def test_reports_backend_response_errors(self):
+        with self.assertRaises(member.BackendParseError), patch_deep_copy("src.test.makeradmin_mock.response", makeradmin_mock.response) as response:
+            response["data"]["membership_data"] = dict()
+            m = member.Member.from_tagid(self.client, 0)
+
+        with self.assertRaises(member.BackendParseError), patch_deep_copy("src.test.makeradmin_mock.response", makeradmin_mock.response) as response:
+            response["data"]["membership_data"]["membership_end"] = "Not a date"
+            m = member.Member.from_tagid(self.client, 0)
+
+    def test_member_not_found(self):
+        with self.assertRaises(member.NoMatchingMemberNumber), patch_deep_copy("src.test.makeradmin_mock.response", makeradmin_mock.response) as response:
+            response["data"] = None
+            m = member.Member.from_member_number(self.client, 1000)
+
+    def test_tag_not_found(self):
+        with self.assertRaises(member.NoMatchingTagId), patch_deep_copy("src.test.makeradmin_mock.response", makeradmin_mock.response) as response:
+            response["data"] = None
+            m = member.Member.from_tagid(self.client, 1000)
