19adb33fdecb3a9372615abc00bd02c2e4951552
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-01-08 14:13:13 +0100

    Make service token persistent on disk

--- memberbooth.py
+++ memberbooth.py
@@ -12,7 +12,6 @@ import config
 from src.gui.states import Application
 import sys
 import traceback
-import os
 
 init_logger("memberbooth")
 logger = get_logger()
@@ -43,7 +42,6 @@ def main():
     parser.add_argument("--input-method", choices=InputMethods, default=InputMethods.EM4100,
                         type=InputMethods.from_string, help="The method to input the key")
 
-    parser.add_argument("--ramdisk-path", default=config.ramdisk_path, help="Path to ramdisk")
     parser.add_argument("--slack-channel-id", help="Channel id for Slack channel")
 
     ns = parser.parse_args()
@@ -63,18 +61,16 @@ def main():
         logger.error(f"Invalid input method: {ns.input_method}")
         sys.exit(-1)
 
-    makeradmin_token_path = os.path.join(ns.ramdisk_path, config.makeradmin_token_filename)
     if no_backend:
         makeradmin_client = MockedMakerAdminClient(base_url=config.maker_admin_base_url,
-                                                   token_path=makeradmin_token_path)
+                                                   token_path=config.makeradmin_token_filename)
     else:
-        makeradmin_client = MakerAdminClient(base_url=ns.maker_admin_base_url, token_path=makeradmin_token_path)
+        makeradmin_client = MakerAdminClient(base_url=ns.maker_admin_base_url, token_path=config.makeradmin_token_filename)
 
-    slack_token_path = os.path.join(ns.ramdisk_path, config.slack_token_filename)
     if no_slack:
-        slack_client = MockSlackClient(token_path=slack_token_path, channel_id=ns.slack_channel_id)
+        slack_client = MockSlackClient(token_path=config.slack_token_filename, channel_id=ns.slack_channel_id)
     else:
-        slack_client = SlackClient(token_path=slack_token_path, channel_id=ns.slack_channel_id, timeout=config.slack_timeout)
+        slack_client = SlackClient(token_path=config.slack_token_filename, channel_id=ns.slack_channel_id, timeout=config.slack_timeout)
 
     app = Application(key_reader_class, makeradmin_client, slack_client)
     try:
