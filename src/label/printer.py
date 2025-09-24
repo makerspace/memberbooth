97e8c8101dae115d90831b0c249a6465c61e41b2
Aron Granberg <aron.granberg@gmail.com>
2025-09-24 22:23:28 +0200

    Work around printer library not being updated for new pillow versions.

--- src/label/printer.py
+++ src/label/printer.py
@@ -3,6 +3,7 @@ from typing import Any, Tuple
 from brother_ql.backends.helpers import send
 from brother_ql.conversion import convert
 from brother_ql.raster import BrotherQLRaster
+from brother_ql.devicedependent import label_type_specs
 from PIL import Image
 import usb.core
 
@@ -34,6 +35,15 @@ def print_label(label: Image.Image) -> dict[str, Any]:
     printer_model, printer = get_printer_config()
     print(printer_model, printer)
     qlr = BrotherQLRaster(printer_model)
+
+    # The brother ql library has conversion functions, but they are not updated
+    # to newer versions of pillow, so they will crash.
+    # Therefore we resize the label ourselves to the correct width.
+    dots_printable = label_type_specs[LABEL_TYPE]['dots_printable']
+    if label.size[0] != dots_printable[0]:
+        hsize = int((dots_printable[0] / label.size[0]) * label.size[1])
+        label = label.resize((dots_printable[0], hsize), Image.LANCZOS)
+
     qlr = convert(qlr, [label], LABEL_TYPE)
 
     return send(instructions=qlr, printer_identifier=printer, backend_identifier=PRINTER_BACKEND, blocking=True)
