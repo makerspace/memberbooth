eb98ca87236ecc9bec7846377f28943324c84f59
Aron Granberg <aron.granberg@gmail.com>
2025-09-25 15:19:07 +0200

    Add interactive mode to print_label.py.

--- print_label.py
+++ print_label.py
@@ -17,10 +17,53 @@ import config
 init_logger("print_label")
 logger = get_logger()
 
+def print_label(member: Member, makeradmin_client: makeradmin.MakerAdminClient | makeradmin_mock.MakerAdminClient, type: str, no_printer: bool = False, description: str | None = None) -> None:
+    match type:
+        case "box":
+            label_data = BoxLabel.from_member(member)
+        case "temp":
+            if description is None:
+                logger.error("You must provide a description for temporary storage labels using --description")
+                return
+            label_data = TemporaryStorageLabel.from_member(member,
+                                                            description=description,
+                                                            expires_at=(datetime.now() + timedelta(days=int(label_creator.TEMP_STORAGE_LENGTH))).date()
+                                                            )
+        case "3d":
+            label_data = Printer3DLabel.from_member(member)
+        case "name":
+            label_data = NameTag.from_member(member)
+        case "meetup":
+            label_data = MeetupNameTag.from_member(member)
+        case "warning":
+            maybe_y = input(color(
+                "Make sure that the yellow label printer paper roll is currently in use.", bg='yellow', fg='black')
+                + "\nSee https://wiki.makerspace.se/Memberbooth for info on how to change the printer paper.\n"
+                "         Type 'y' to continue, or anything else to exit. ")
+            if maybe_y.lower() != 'y':
+                print("Exiting")
+                return
+
+            # TODO: Use logged in member
+            label_data = WarningLabel.from_member(member, description=description, expires_at=(datetime.now() + timedelta(days=int(label_creator.TEMP_WARNING_STORAGE_LENGTH))).date())
+
+    uploaded_label = makeradmin_client.post_label(label_data)
+    label = label_creator.create_label(uploaded_label)
+
+    if no_printer:
+        file_name = f'{member.member_number}_{type}_{str(int(time()))}.png'
+        logger.info(
+            f'Program run with --no-printer, storing label image to {file_name} instead of printing it.')
+        print(f"Saving box label to {file_name}")
+        label.save(file_name)
+        label.show()
+    else:
+        label_printer.print_label(label.label)
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
+    group2 = parser.add_mutually_exclusive_group()
     parser.add_argument("--type", choices=["box", "temp", "3d", "warning", "name", "meetup"], default="name")
     group.add_argument("-t", "--token_path", help="Path to Makeradmin token.", default=config.makeradmin_token_filename)
     group.add_argument("--development", action="store_true", help="Mock events")
@@ -30,11 +73,12 @@ def main() -> None:
                         help="Base url of maker admin backend")
     parser.add_argument("--no-printer", action="store_true", help="Mock label printer (save label to file instead)")
 
-    parser.add_argument('member_numbers',
+    group2.add_argument('member_numbers',
                         type=int,
-                        nargs='+',
+                        nargs='*',
                         help='The member number(s) of the member(s) you want to print a label for')
-    
+    group2.add_argument('--interactive', action='store_true', help='Ask before printing each label')
+
     parser.add_argument('--description', type=str, help='Description to put on temporary storage labels', default=None)
 
     ns = parser.parse_args()
@@ -52,54 +96,37 @@ def main() -> None:
         logger.warning("The makeradmin client is not logged in")
         makeradmin_client.login()
 
-    for member_number in ns.member_numbers:
-        try:
-            member = Member.from_member_number(makeradmin_client, member_number)
-        except NoMatchingMemberNumber:
-            logger.error(f"Member number {member_number} did not match any known member")
-        else:
+    if ns.interactive:
+        while True:
+            try:
+                input_str = input(color("Enter member number: ", fg='orange'))
+                if input_str.lower() == 'q':
+                    print(color("Exiting interactive mode.", fg='yellow'))
+                    exit(0)
+                member_number = int(input_str)
+            except ValueError:
+                print(color("Invalid input. Please enter a valid member number.", fg='red'))
+                continue
 
-            match ns.type:
-                case "box":
-                    label_data = BoxLabel.from_member(member)
-                case "temp":
-                    if ns.description is None:
-                        logger.error("You must provide a description for temporary storage labels using --description")
-                        continue
-                    label_data = TemporaryStorageLabel.from_member(member,
-                                                                   description=ns.description,
-                                                                   expires_at=(datetime.now() + timedelta(days=int(label_creator.TEMP_STORAGE_LENGTH))).date()
-                                                                   )
-                case "3d":
-                    label_data = Printer3DLabel.from_member(member)
-                case "name":
-                    label_data = NameTag.from_member(member)
-                case "meetup":
-                    label_data = MeetupNameTag.from_member(member)
-                case "warning":
-                    maybe_y = input(color(
-                        "Make sure that the yellow label printer paper roll is currently in use.", bg='yellow', fg='black')
-                        + "\nSee https://wiki.makerspace.se/Memberbooth for info on how to change the printer paper.\n"
-                        "         Type 'y' to continue, or anything else to exit. ")
-                    if maybe_y.lower() != 'y':
-                        print("Exiting")
-                        return
+            try:
+                member = Member.from_member_number(makeradmin_client, member_number)
+            except NoMatchingMemberNumber:
+                print(color(f"Member number {member_number} did not match any known member", fg='red'))
+                continue
 
-                    # TODO: Use logged in member
-                    label_data = WarningLabel.from_member(member, description=ns.description, expires_at=(datetime.now() + timedelta(days=int(label_creator.TEMP_WARNING_STORAGE_LENGTH))).date())
+            maybe_y = input(color(f"Print {ns.type} label for", fg="orange") + color(" " + member.get_name(), style='bold') + color(f" (#{member.member_number})? (y/n) ", fg='orange'))
+            if maybe_y.lower() == 'y':
+                print_label(member, makeradmin_client, ns.type, ns.no_printer, ns.description)
+    else:
+        if not ns.member_numbers:
+            print(color("No member numbers provided. Use --interactive to enter member numbers one by one, or provide member numbers as arguments.", fg='red'))
 
-            uploaded_label = makeradmin_client.post_label(label_data)
-            label = label_creator.create_label(uploaded_label)
-
-            if ns.no_printer:
-                file_name = f'{member.member_number}_{ns.type}_{str(int(time()))}.png'
-                logger.info(
-                    f'Program run with --no-printer, storing label image to {file_name} instead of printing it.')
-                print(f"Saving box label to {file_name}")
-                label.save(file_name)
-                label.show()
-            else:
-                label_printer.print_label(label.label)
+        for member_number in ns.member_numbers:
+            try:
+                member = Member.from_member_number(makeradmin_client, member_number)
+            except NoMatchingMemberNumber:
+                logger.error(f"Member number {member_number} did not match any known member")
+            print_label(member, makeradmin_client, ns.type, ns.no_printer, ns.description)
 
 
 if __name__ == "__main__":
